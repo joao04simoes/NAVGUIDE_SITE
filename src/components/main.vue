7ace2c41bd6d531186153906b8bdb1760653881e
joao simoes <joao.simoes@tecnico.ulisboa.pt>
2025-03-03 16:37:14 +0000

    dev joao

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main-container">
         <h1>Bem-vindo ao NAVGUIDE</h1>
-        <p>Este site é uma bosta.</p>
+        <p>Este site é bonito. xx</p>
 
         <button @click="$router.push('/Blog')">Ir para o Blog</button>
     </div>
