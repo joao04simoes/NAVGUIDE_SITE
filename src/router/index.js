ed15a092114eb55c1e62c4f601668ec040d62a92
joao simoes <joao.simoes@tecnico.ulisboa.pt>
2025-03-04 21:27:12 +0000

    remover bussola

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,13 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 // Import components
 import Blog from '../components/BLOG.vue';
 import main from '../components/main.vue';
-import BUSS from '../components/BUSS.vue';
+
 
 // Define routes
 const routes = [
     { path: '/', name: 'main', component: main },
     { path: '/Blog', name: 'Blog', component: Blog },
-    { path: '/BUSS', name: 'BUSS', component: BUSS, }
+
 ];
 
 // Create the router instance
