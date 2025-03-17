781a5f43a590abf17c8e706e537f5b1e8a6688de
joao simoes <joao.simoes@tecnico.ulisboa.pt>
2025-03-17 23:04:42 +0000

    logo

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -28,20 +28,20 @@ const scrollToSection = (id) => {
     <div class="container mx-auto px-5">
         <header class="flex justify-between py-6 items-center">
             <!-- Logo -->
-            <div class="font-bold text-lg">
-                NAVGUIDE
+            <div class="flex items-center">
+                <img src="/images/LogoNavGuide.png" alt="NAVGUIDE Logo" class="h-12 md:h-16 object-contain">
             </div>
 
-            <!-- Menu Toggler (Mobile) -->
-            <button @click="toggleMobileNav" type="button" class="flex md:hidden focus:outline-none">
-                <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
+            <!-- Botão Menu Mobile -->
+            <button @click="toggleMobileNav" type="button" class="md:hidden focus:outline-none">
+                <svg viewBox="0 0 24 24" class="h-8 w-8 text-white">
                     <path fill-rule="evenodd"
                         d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2z">
                     </path>
                 </svg>
             </button>
 
-            <!-- Desktop Menu -->
+            <!-- Menu Desktop -->
             <nav class="hidden md:flex items-center">
                 <ul class="flex space-x-10 font-medium">
                     <li class="uppercase hover:text-theme-secondary transition duration-200">
@@ -60,28 +60,28 @@ const scrollToSection = (id) => {
     </div>
 
     <!-- Mobile Menu -->
-    <div v-show="mobileNav"
-        class="absolute z-30 top-0 left-0 w-full h-full bg-theme-dark-blue flex flex-col items-center justify-center px-5">
-        <button @click="toggleMobileNav()" type="button" class="absolute top-5 right-5 focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                class="bi bi-x text-white" viewBox="0 0 16 16">
-                <path
-                    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+    <div v-if="mobileNav"
+        class="fixed top-0 left-0 w-full h-screen bg-theme-dark-blue flex flex-col items-center justify-center z-50">
+        <!-- Botão para fechar -->
+        <button @click="toggleMobileNav" class="absolute top-5 right-5">
+            <svg class="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
         </button>
 
-        <ul class="text-white text-lg space-y-6 text-center">
+        <!-- Links do menu -->
+        <ul class="text-white text-3xl space-y-8 text-center">
             <li>
-                <router-link to="/" @click.native="scrollToSection('team')"
-                    class="hover:text-theme-secondary transition duration-200">TEAM</router-link>
+                <a @click="scrollToSection('team')" class="cursor-pointer hover:text-gray-400 transition">TEAM</a>
             </li>
             <li>
-                <router-link to="/" @click.native="scrollToSection('tecSol')"
-                    class="hover:text-theme-secondary transition duration-200">SOLUTION</router-link>
+                <a @click="scrollToSection('tecSol')" class="cursor-pointer hover:text-gray-400 transition">SOLUTION</a>
             </li>
-            <li
-                class="bg-theme-secondary px-6 py-2 text-white rounded shadow-md hover:bg-white border-2 border-transparent hover:border-theme-secondary hover:text-theme-secondary cursor-pointer transition duration-200">
-                <router-link to="/blog">BLOG</router-link>
+            <li>
+                <router-link to="/blog" class="bg-red-500 text-white px-6 py-3 rounded-lg hover:bg-red-600 transition">
+                    BLOG
+                </router-link>
             </li>
         </ul>
     </div>
