291a1496270c444b61ca2dd44df2c90f1707585e
joao simoes <joao.simoes@tecnico.ulisboa.pt>
2025-04-09 19:33:56 +0100

    add not found page

---
+++ src/layouts/notFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-col h-[480px]">
+        <!-- Conteúdo principal -->
+        <main class="flex-grow flex flex-col justify-center items-center text-center p-10">
+            <h1 class="text-4xl font-bold text-red-600 mb-4">404 - Página não encontrada</h1>
+            <p class="mb-6">O link que seguiste não existe.</p>
+            <router-link to="/" class="text-blue-600 underline">Voltar à página principal</router-link>
+        </main>
+    </div>
+</template>
