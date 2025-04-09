9159a5845acefac3e7921458519373d5ef743ab4
joao simoes <joao.simoes@tecnico.ulisboa.pt>
2025-04-09 18:54:36 +0100

    add person counter

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -10,7 +10,6 @@ import team from "../sections/team.vue";
 import Partners from "../sections/Partners.vue";
 import pdf from "../sections/pdf.vue";
 import counter from "../sections/counter.vue";
-import counter from "../sections/counter.vue";
 
 </script>
 
